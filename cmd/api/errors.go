894d7b62baa78a47b89f870acb37f13ab0ab21ec
robertgouveia <robertgouveia03@hotmail.com>
2024-10-25 15:07:41 +0100

    Handling errors

---
+++ cmd/api/errors.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func (app *application) internalServerError(w http.ResponseWriter, r *http.Request, err error) {
+	log.Printf("internal server error: %s path: %s error: %s", r.Method, r.URL.Path, err)
+
+	writeJSONError(w, http.StatusInternalServerError, "The server encountered a problem")
+}
+
+func (app *application) badRequest(w http.ResponseWriter, r *http.Request, err error) {
+	writeJSONError(w, http.StatusBadRequest, err.Error())
+}
+
+func (app *application) notFound(w http.ResponseWriter, r *http.Request, err error) {
+	writeJSONError(w, http.StatusNotFound, "Resource not found")
+}
