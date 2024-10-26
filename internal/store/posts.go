e3c9057e4572f199c5d294374c248a0ded2b4fb4
robertgouveia <robertgouveia03@hotmail.com>
2024-10-26 12:59:08 +0100

    Filtering

--- internal/store/posts.go
+++ internal/store/posts.go
@@ -131,12 +131,12 @@ func (s *PostStore) Update(ctx context.Context, post *Post) error {
 
 func (s *PostStore) GetUserFeed(ctx context.Context, userID int64, fq PaginatedFeedQuery) ([]PostWithMetaData, error) {
 	query := `
-		SELECT p.id, p.user_id, p.title, p.created_at, p.version, p.tags, u.username, COUNT(c.id) AS comments_count
+		SELECT p.id, p.user_id, p.title, p.content, p.created_at, p.version, p.tags, u.username, COUNT(c.id) AS comments_count
 		FROM public.posts p
 		LEFT JOIN comments c ON c.post_id = p.id
 		LEFT JOIN users u ON p.user_id = u.id
 		JOIN followers f ON f.follower_id = p.user_id OR p.user_id = $1
-		WHERE f.user_id = $1 OR p.user_id = $1
+		WHERE f.user_id = $1 AND (p.title ILIKE '%' || $4 || '%' OR p.content ILIKE '%'|| $4) AND (p.tags @> $5 OR $5 = '{}')
 		GROUP BY p.id, u.username
 		ORDER BY p.created_at ` + fq.Sort + `
 		LIMIT $2 OFFSET $3
@@ -145,7 +145,7 @@ func (s *PostStore) GetUserFeed(ctx context.Context, userID int64, fq PaginatedF
 	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
 	defer cancel()
 
-	rows, err := s.db.QueryContext(ctx, query, userID, fq.Limit, fq.Offset)
+	rows, err := s.db.QueryContext(ctx, query, userID, fq.Limit, fq.Offset, fq.Search, pq.Array(fq.Tags))
 	if err != nil {
 		return nil, err
 	}
@@ -154,7 +154,7 @@ func (s *PostStore) GetUserFeed(ctx context.Context, userID int64, fq PaginatedF
 	var feed []PostWithMetaData
 	for rows.Next() {
 		var post PostWithMetaData
-		err := rows.Scan(&post.ID, &post.User.ID, &post.Title, &post.CreatedAt, &post.Version, pq.Array(&post.Tags), &post.User.Username, &post.CommentCount)
+		err := rows.Scan(&post.ID, &post.User.ID, &post.Title, &post.Content, &post.CreatedAt, &post.Version, pq.Array(&post.Tags), &post.User.Username, &post.CommentCount)
 		if err != nil {
 			return nil, err
 		}
